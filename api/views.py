885673da16566519342c21cbe466d446ac10e1d1
storrellas <storrellas@gmail.com>
2021-12-03 09:31:03 +0100

    ENH: Adding first endpoints

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, serializers
+
+from .models import Company
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
+
+class CompanyViewSet(viewsets.ReadOnlyModelViewSet):
+  queryset = Company.objects.all()
+  serializer_class = CompanySerializer
