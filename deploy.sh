85c5bc131c7d703e23ce403ac890c64bbb38985a
storrellas <storrellas@gmail.com>
2021-12-15 07:15:36 +0100

    ENH: Updated deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 git pull origin master
 cd docker
 #sudo docker-compose build --no-cache 
-sudo docker-compose build
-sudo docker-compose down
-sudo docker-compose up -d
+docker-compose build
+docker-compose down
+docker-compose up -d
